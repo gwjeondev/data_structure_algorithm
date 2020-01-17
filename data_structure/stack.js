5d9d751797cbc3c095dff986ba98e77431d76a23
won-developer <gw4609@gmail.com>
2020-01-17 17:06:56 +0900

    no message

---
+++ data_structure/stack.js
@@ -0,0 +1,28 @@
+const Stack = function () {
+    
+    let stack = [];
+
+    this.item = stack;
+
+    this.pop = function () {
+        this.item.pop();
+    }
+    this.push = function (element) {
+        this.item.push(element);
+    }
+    this.peek = function () {
+        return this.item[this.item.length - 1];
+    }
+    this.isEmpty = function () {
+        return this.item.length === 0;
+    }
+    this.clear = function () {
+        this.item = [];
+    }
+    this.print = function () {
+        console.log(this.item.toString());
+    }
+    this.indexOf = function (element) {
+        return this.item.indexOf(element);
+    }
+}
