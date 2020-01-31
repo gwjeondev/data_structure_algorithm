6f38f88c8aa97c680099e3ca30b0f0ea72c994de
won-developer <gw4609@gmail.com>
2020-01-31 15:42:31 +0900

    2020 01 31 set

--- data_structure/set.js
+++ data_structure/set.js
@@ -34,4 +34,54 @@ const Set = function() {
   this.values = function() {
       return Object.keys(items);
   }
+  
+  this.union = function(otherSet) { // 합집합
+    const unionSet = new Set();
+
+    let values = this.values();
+    for(index in values) {
+        unionSet.add(values[index]);
+    }
+    values = otherSet.values();
+    for(index in values) {
+        unionSet.add(values[index]);
+    }
+    return unionSet;
+  }
+
+  this.intsec = function(otherSet) { // 교집합
+    const intsecSet = new Set();
+
+    let values = this.values();
+      for(index in values) {
+          if(otherSet.has(values[index])) {
+            intsecSet.add(values[index]);
+          }
+      }
+      return intsecSet;
+}
+  this.diff = function(otherSet) { // 차집합
+      const diffSet = new Set();
+
+      let values = this.values();
+        for(index in values) {
+            if(!otherSet.has(values[index])) {
+                diffSet.add(values[index]);
+            }
+        }
+        return diffSet;
+  }
+
+  this.subset = function(otherSet) { // 부분집합
+    const subsetSet = new Set();
+
+    let values = otherSet.values();
+
+    for(index in values) {
+        if(!this.has(values[index])) {
+            return false;
+        }
+    }
+    return true;
+  }
 };
